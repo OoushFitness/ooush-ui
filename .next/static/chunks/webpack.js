e90269fa039c86517526c9372ef64874df02d446
Alex Green <alexander.green1989@hotmail.com>
2022-08-16 23:38:01 +0100

    update next files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b73cd448f826910b"; }
+/******/ 		__webpack_require__.h = function() { return "1af8111b232ce279"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
